ae68c2574b2ecdac59aa20963df7fa93ae3b5251
InBrewJ <jason.brewer101@gmail.com>
2016-11-27 00:52:30 +0000

    First Commit (MK II)

---
+++ CMakeFiles/JibsInferno.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JibsInferno.dir/src/main.cpp.o"
+  "bin/JibsInferno.pdb"
+  "bin/JibsInferno"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JibsInferno.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "Welcome to the BrewEngine motherfucker! You're in for a bumpy ride" << endl;
+  
+}
