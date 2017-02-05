9ecf249fd1ed8fd297baec347c290e2692acb3a1
InBrewJ <jason.brewer101@gmail.com>
2017-02-05 17:49:52 +0000

    Funky Cube

--- src/Game.cpp
+++ src/Game.cpp
@@ -162,14 +162,92 @@ int Game::play() {
 
 	printf("After bind vertex array\n");
 
-	// An array of 3 vectors which represents 3 vertices
-
+	// Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+	
 	static const GLfloat g_vertex_buffer_data[] = {
-		-0.05f, -0.05f, 0.0f,
-		0.05f, -0.05f, 0.0f,
-		0.0f, 0.05f, 0.0f
+		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, // triangle 1 : end
+		1.0f, 1.0f,-1.0f, // triangle 2 : begin
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f, // triangle 2 : end
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
 	};
 
+	static const GLfloat g_color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	
 	// This will identify out vertex buffer
 	GLuint vertexbuffer;
 	
@@ -188,6 +266,18 @@ int Game::play() {
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// 2nd attribute buffer : colors
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glVertexAttribPointer(
+						  1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+						  3,                                // size
+						  GL_FLOAT,                         // type
+						  GL_FALSE,                         // normalized?
+						  0,                                // stride
+						  (void*)0                          // array buffer offset
+						  );
+
 	// Projection matrix: 45 degree field of view, 16:9(?) ratio, display range:
 	// 0.1 unit <-> 100 units
 
@@ -195,7 +285,7 @@ int Game::play() {
 
 	// Camera Matrix
 	mat4 View = lookAt(
-					   vec3(0,0,-1), // Camera is a (0,0,-1) in world space
+					   vec3(4,3,3), // Camera is a (0,0,-1) in world space
 					   vec3(0,0,0), // and looks at the origin
 					   vec3(0,1,0)  // Head is up (right hand rule)
 					   );
@@ -208,9 +298,9 @@ int Game::play() {
 	// TRANSLATE
 	
 	mat4 Model = mat4(1.0f);
-	vec3 myRotationAxis( 0, 0, 1);
-	Model = translate(vec3(0.4f, 0.3f, 0.0f));
-	Model = rotate( Model, 30.0f, myRotationAxis );
+	//vec3 myRotationAxis( 0, 0, 0);
+	Model = translate(vec3(0.0f, 0.0f, 0.0f));
+	//Model = rotate( Model, 0.0f, myRotationAxis );
 
 	// Our ModelViewProjection : multiplication of our 3 matrices
 	mat4 mvp = Projection * View * Model;
@@ -259,7 +349,7 @@ int Game::play() {
 		// Use our shader
 		glUseProgram(programID);
 		// Draw the triangle!
-		glDrawArrays(GL_TRIANGLES, 0, 3); // starting from vertex 0, 3 vertices total -> 1 triangle
+		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
 		glDisableVertexAttribArray(0);
 		
 		// Swap buffers
--- src/SimpleFragmentShader.fragmentshader.glsl
+++ src/SimpleFragmentShader.fragmentshader.glsl
@@ -1,5 +1,12 @@
 #version 330 core
+
+in vec3 fragmentColor;
+
+// Ouput data
 out vec3 color;
+
 void main(){
-  color = vec3(0,0.7,0);
+    // Output color = color specified in the vertex shader,
+    // interpolated between all 3 surrounding vertices
+    color = fragmentColor;
 }
